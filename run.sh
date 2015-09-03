74d7a4ce49cf14f5cc71abadd686b723c4a0766f
stevenbower <smb-github@alcyon.net>
2015-09-02 22:46:27 -0400

    fixed up run script for networking

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -h 10.10.22.30 -v /opt/plex/conf:/config -v /opt/plex/data:/data -p 32400:32400 plex
+docker run -d --net=host -v /opt/plex/conf:/config -v /opt/plex/data:/data -p 32400:32400 plex
